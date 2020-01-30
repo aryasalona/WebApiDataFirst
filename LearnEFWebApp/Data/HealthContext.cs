01ecea71f6521ca5262feaf7dd89b333170ef389
Sanjay Singh <sanjay@minfytech.com>
2020-01-30 11:09:25 +0530

    Add all the files for both web api and client project

---
+++ LearnEFWebApp/Data/HealthContext.cs
@@ -0,0 +1,29 @@
+﻿using LearnEFWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnEFWebApp.Data
+{
+	public class HealthContext : DbContext
+	{
+		
+		public HealthContext(DbContextOptions options)
+		: base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+		}
+
+		public DbSet<Patient>  Patients { get; set; }
+		public DbSet<Ailment> Ailments { get; set; }
+		public DbSet<Medication> Medications { get; set; }
+	
+
+	}
+}
